efa744326fb98680a02f21714535248183502fd8
Chaoses-Ib <Chaos-es@outlook.com>
2025-07-09 21:40:38 +0800

    chore(bench): update criterion to v0.6

--- ib-matcher/benches/matcher.rs
+++ ib-matcher/benches/matcher.rs
@@ -1,4 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, Criterion};
 use ib_matcher::{matcher::PinyinMatcher, pinyin::PinyinNotation};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
