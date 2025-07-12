d3f1b181246c99194d77747565bc74e7cf68e9ac
Chaoses-Ib <Chaos-es@outlook.com>
2025-07-12 17:29:29 +0800

    fix(ahk2): publish.ps1

--- ib-pinyin/bindings/ahk2/build.ps1
+++ ib-pinyin/bindings/ahk2/build.ps1
@@ -3,7 +3,7 @@ if (!$?) {
     throw "Build ib-pinyin-c failed"
 }
 
-$64DllSrc = Join-Path -Path $PSScriptRoot -ChildPath ../../target/release/ib_pinyin_c.dll
+$64DllSrc = Join-Path -Path $PSScriptRoot -ChildPath ../../../target/release/ib_pinyin_c.dll
 $64DllDst = Join-Path -Path $PSScriptRoot -ChildPath Lib/IbPinyin64.dll
 Copy-Item -Path $64DllSrc -Destination $64DllDst -Force
 
@@ -14,7 +14,7 @@ if (!$?) {
     throw "Build 32-bit ib-pinyin-c failed"
 }
 
-$32DllSrc = Join-Path -Path $PSScriptRoot -ChildPath ../../target/i686-pc-windows-msvc/release/ib_pinyin_c.dll
+$32DllSrc = Join-Path -Path $PSScriptRoot -ChildPath ../../../target/i686-pc-windows-msvc/release/ib_pinyin_c.dll
 $32DllDst = Join-Path -Path $PSScriptRoot -ChildPath Lib/IbPinyin32.dll
 Copy-Item -Path $32DllSrc -Destination $32DllDst -Force
 
--- ib-pinyin/bindings/ahk2/publish.ps1
+++ ib-pinyin/bindings/ahk2/publish.ps1
@@ -1,5 +1,5 @@
 ﻿& (Join-Path -Path $PSScriptRoot -ChildPath build.ps1)
 
-$publishDir = Join-Path -Path $PSScriptRoot -ChildPath ../../target/publish
-$publishZip = Join-Path -Path $publishDir -ChildPath IbPinyinLib.AHK2.zip
+$publishDir = Join-Path -Path $PSScriptRoot -ChildPath ../../../target/publish
+$publishZip = Join-Path -Path $publishDir -ChildPath ib-pinyin-ahk2.zip
 Compress-Archive -Path (Join-Path -Path $PSScriptRoot -ChildPath Lib) -DestinationPath $publishZip -Force
